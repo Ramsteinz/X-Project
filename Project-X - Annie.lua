73c779f705578e1c62aad1af161ab975a788d88c
Ramsteinz <lol@cgocable.ca>
2014-09-16 13:55:16 -0400

    Update Project-X - Annie.lua

--- Project-X - Annie.lua
+++ Project-X - Annie.lua
@@ -1,14 +1,56 @@
-if not myHero.charName ~= "Annie" then return end
 local version = 1.01
 
-local scriptName = "Project-X - Annie"
-printMessage = function(message) print("<font color=\"#6699FF\"><b>"..scriptName..":</b></font> <font color=\"#FFFFFF\">"..message.."</font>") end
-if FileExist(LIB_PATH.."SourceLib.lua") then
-  require 'SourceLib'
-else
-  printMessage("Downloading SourceLib, please wait ...")
-  DownloadFile("https://raw.github.com/LegendBot/Scripts/master/Common/SourceLib.lua",LIB_PATH.."SourceLib.lua", function() printMessage("SourceLib successfully downloadded, please reload (double [F9]).") end)
-  return true
+_G.UseUpdater = true
+
+local REQUIRED_LIBS = {
+  ["SOW"] = "https://raw.githubusercontent.com/Ramsteinz/X-Project/master/SOW.lua",
+}
+
+local DOWLOADING_LIBS, DOWNLOAD_COUNT = false, 0
+
+function AfterDownload()
+  DOWNLOAD_COUNT = DOWNLOAD_COUNT - 1
+  if DOWNLOAD_COUNT == 0 then
+    DOWNLOAD_LIBS = false
+    print("<b><font color=\"#6699FF\">X-Project - Annie:</font></b> <font color=\"#FFFFFF\">Required libraries downloaded successfully, please reload (double F9).</font>")
+  end
+end
+
+for DOWNLOAD_LIB_NAME, DOWNLOAD_LIB_URL in pairs(REQUIRED_LIBS) do
+  if FileExist(LIB_PATH .. DOWNLOAD_LIB_NAME .. ".lua") then
+    require(DOWNLOAD_LIB_NAME)
+  else
+    DOWNLOADING_LIBS = true
+    DOWNLOAD_COUNT = DOWNLOAD_COUNT + 1
+    DownloadFile(DOWNLOAD_LIB_URL, LIB_PATH .. DOWNLOAD_LIB_NAME..".lua", AfterDownload)
+  end
+end
+
+local UPDATE_NAME = "X-Project - Annie"
+local UPDATE_HOST = "raw.githubusercontent.com"
+local UPDATE_PATH ="/Ramsteinz/X-Project/master/Project-X%20-%20Annie.lua" .. "?rand=" .. math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+
+function AutoupdaterMsg(msg) print("<b><font color=\"#6699FF\">"..UPDATE_NAME..":</font></b> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if _G.UseUpdater then
+  local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+  if ServerData then
+    local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+    ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+    if ServerVersion then
+      ServerVersion = tonumber(ServerVersion)
+      if tonumber(version) < ServerVersion then
+        AutoupdaterMsg("New version available "..ServerVersion)
+        AutoupdaterMsg("Updating, please don't press F9")
+        DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMSG("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)
+      else
+        AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+      end
+    end
+  else
+    AutoupdaterMsg("Error downloading version info")
+  end
 end
 
 -- called once when the script is loaded
